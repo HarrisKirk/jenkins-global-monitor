4a4000acacdd375746b04891cac06639aee8bc05
Harris Kirk <hw.kirk@gmail.com>
2013-10-15 07:43:26 -0400

    Ensure all test case reflect servers that are not functional

--- src/test/resources/DynamicConfiguration.groovy
+++ src/test/resources/DynamicConfiguration.groovy
@@ -2,9 +2,9 @@ public class DynamicConfiguration implements DynamicConfigurationInterface {
 	
 	public def getInstanceList() {
 		[
-			new JenkinsInstanceSpecification( '114', "http://192.168.1.114:8080" ),
-			new JenkinsInstanceSpecification( '139', "http://192.168.1.139:8080" ),
-			new JenkinsInstanceSpecification( '63', "http://192.168.1.63:8080" ),
+			new JenkinsInstanceSpecification( '111', "http://192.168.1.114:8080" ),
+			new JenkinsInstanceSpecification( '111', "http://192.168.1.139:8080" ),
+			new JenkinsInstanceSpecification( '11', "http://192.168.1.63:8080" ),
 		]
 	}
 	
@@ -20,14 +20,14 @@ public class DynamicConfiguration implements DynamicConfigurationInterface {
 		[
 		'Pipeline 1 -->': 
 			[
-			new JenkinsJobSpecification( '252:Installer', 'http://192.168.1.252:8080/jenkins/job/spectrum-installer-SNAPSHOT'),
-			new JenkinsJobSpecification( '142:LATEST-WEBLOGIC', 'http://192.168.1.142:8080/jenkins/job/LATEST-WEBLOGIC'),
-			new JenkinsJobSpecification( '166:LATEST-WEBLOGIC', 'http://192.168.1.166:8080/jenkins/job/LATEST-WEBLOGIC'),
+			new JenkinsJobSpecification( '111:Installer', 'http://192.168.1.111:8080/jenkins/job/spectrum-installer-SNAPSHOT'),
+			new JenkinsJobSpecification( '111:LATEST-WEBLOGIC', 'http://192.168.1.111:8080/jenkins/job/LATEST-WEBLOGIC'),
+			new JenkinsJobSpecification( '111:LATEST-WEBLOGIC', 'http://192.168.1.111:8080/jenkins/job/LATEST-WEBLOGIC'),
 			],
 		'Pipeline 2 -->': 
 			[
-			new JenkinsJobSpecification( '252:Tagged Installer', 'http://192.168.1.252:8080/jenkins/job/spectrum-installer-99_9_9_xxx'),
-			new JenkinsJobSpecification( '139:Latest Perf', 'http://192.168.1.139:8080/jenkins/job/install-latest-perf'),
+			new JenkinsJobSpecification( '111:Tagged Installer', 'http://192.168.1.111:8080/jenkins/job/spectrum-installer-99_9_9_xxx'),
+			new JenkinsJobSpecification( '111:Latest Perf', 'http://192.168.1.111:8080/jenkins/job/install-latest-perf'),
 			],
 		]
 	}
