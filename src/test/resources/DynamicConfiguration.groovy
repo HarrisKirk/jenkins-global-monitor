23498a580c0e0a0ec1137b15667296227df2a780
Harris Kirk <Harris.Kirk@wolterskluwer.com>
2016-04-01 15:05:01 -0400

    Add simple working external jenkins site

--- src/test/resources/DynamicConfiguration.groovy
+++ src/test/resources/DynamicConfiguration.groovy
@@ -2,9 +2,7 @@ public class DynamicConfiguration implements DynamicConfigurationInterface {
 	
 	public def getInstanceList() {
 		[
-			new JenkinsInstanceSpecification( '111', "http://192.168.1.114:8080" ),
-			new JenkinsInstanceSpecification( '111', "http://192.168.1.139:8080" ),
-			new JenkinsInstanceSpecification( '11', "http://192.168.1.63:8080" ),
+			new JenkinsInstanceSpecification( '111', "http://ci.zikula.org/" ),
 		]
 	}
 	
@@ -17,19 +15,7 @@ public class DynamicConfiguration implements DynamicConfigurationInterface {
 	}
 	
 	public def getPipelineSpecs() {
-		[
-		'Pipeline 1 -->': 
-			[
-			new JenkinsJobSpecification( '111:Installer', 'http://192.168.1.111:8080/jenkins/job/spectrum-installer-SNAPSHOT'),
-			new JenkinsJobSpecification( '111:LATEST-WEBLOGIC', 'http://192.168.1.111:8080/jenkins/job/LATEST-WEBLOGIC'),
-			new JenkinsJobSpecification( '111:LATEST-WEBLOGIC', 'http://192.168.1.111:8080/jenkins/job/LATEST-WEBLOGIC'),
-			],
-		'Pipeline 2 -->': 
-			[
-			new JenkinsJobSpecification( '111:Tagged Installer', 'http://192.168.1.111:8080/jenkins/job/spectrum-installer-99_9_9_xxx'),
-			new JenkinsJobSpecification( '111:Latest Perf', 'http://192.168.1.111:8080/jenkins/job/install-latest-perf'),
-			],
-		]
+		return []
 	}
 	
 }
